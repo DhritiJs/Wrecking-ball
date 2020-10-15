fe92115167a09b2be96e8ac9b11932d9c77fae97
DhritiJs <70759231+DhritiJs@users.noreply.github.com>
2020-10-15 16:55:31 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class box{
+    constructor(x,y,width,height){
+var options={
+    restitution :0.8,
+    friction:1.0,
+    density:0.04
+}
+this.x=x;
+this.y=y;
+this.width=width;
+this.height=height;
+this.body=Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("red");
+        fill("white");
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
